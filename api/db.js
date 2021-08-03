0f20bd4b1b68bb8169cf5c723c49fee2e299b562
Matias Campodonico <77130726+matiascamp@users.noreply.github.com>
2021-08-03 18:24:47 -0300

    db completo junto con relaciones de tablas

--- api/db.js
+++ api/db.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const { DataTypes } = require("sequelize")
 const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
@@ -28,13 +29,102 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Bundle,Category,Image,Office,Order,Product,Review,Schedule,Stock,User,Wishlist } = sequelize.models;
+const { Bundle,Category,Image,Office,Order,Product,Review,Schedule,Stock,User,Wishlist,BuyHistory } = sequelize.models;
 
-Bundle.belongsToMany(Product,{through: "bundle_product"});
-Product.belongsToMany(Bundle,{through: "bundle_product"});
+BuyHistory.belongsToMany(User,{through:"buyhistory_user"});
+User.belongsTo(BuyHistory,{foreignKey:"user_id"});
+
+BuyHistory.belongsToMany(Order,{through:"buyhistory_order"});
+Order.belongsToMany(BuyHistory,{through:"buyhistory_order"});
+
+const Bundle_Product = sequelize.define('Bundle_Product', {
+  id: {type: DataTypes.UUID,
+  defaultValue: DataTypes.UUIDV4,
+  allowNull: false,
+  primaryKey: true},
+  quantity: DataTypes.INTEGER
+});
+
+
+Bundle.belongsToMany(Product,{ through: Bundle_Product });
+Product.belongsToMany(Bundle,{ through: Bundle_Product });
+
+
+const Category_Product = sequelize.define('Category_Product', {
+  id: {type: DataTypes.UUID,
+  defaultValue: DataTypes.UUIDV4,
+  allowNull: false,
+  primaryKey: true}
+});
+
+
+Category.belongsToMany(Product,{ through: Category_Product});
+Product.belongsToMany(Category,{ through: Category_Product});
+
+Product.hasMany(Image);
+Image.belongsTo(Product);
+
+
+const Order_Product = sequelize.define('Order_Product', {
+  id: {type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true},
+    quantity: DataTypes.INTEGER,
+});
+
+
+Product.belongsToMany(Order,{through: Order_Product});
+Order.belongsToMany(Product,{through: Order_Product});
+
+const Order_Schedule = sequelize.define('Order_Schedule', {
+  id: {type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true},
+});
+
+
+Order.belongsToMany(Schedule, {through: Order_Schedule});
+Schedule.belongsToMany(Order, {through: Order_Schedule});
+
+Product.belongsToMany(Stock,{through:"product_stock"});
+Stock.belongsTo(Product,{foreignKey:"product_id"})
+
+Product.belongsToMany(Wishlist, {through:"product_wishlist"});
+Wishlist.belongsTo(Product,{foreignKey:"product_id"})
+
+User.hasOne(Wishlist);
+Wishlist.belongsTo(User);
+
+Wishlist.belongsToMany(Stock,{through:"wishlist_stock"});
+Stock.belongsToMany(Wishlist,{through:"wishlist_stock"});
+
+Office.belongsToMany(Stock,{through:"office_stock"});
+Stock.belongsTo(Office,{foreignKey:"office_id"})
+
+Office.belongsToMany(Schedule,{through:"office_schedule"});
+Schedule.belongsTo(Office,{foreignKey:"office_id"})
+
+User.belongsToMany(Order,{through:"user_order"});
+Order.belongsTo(User,{foreignKey:"user_id"});
+
+User.belongsToMany(Review,{through:"user_review"});
+Review.belongsTo(User,{foreignKey:"reviewer_id"});
+
+Order.belongsToMany(Review,{through:"order_review"});
+Review.belongsTo(Order,{foreignKey:"order_id"})
+
+User.belongsToMany(Schedule,{through:"user_schedule"});
+Schedule.belongsTo(User,{foreignKey:"admin_id"})
+
+
+
 
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
-};
+}
+
+
