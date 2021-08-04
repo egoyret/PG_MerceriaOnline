fc79a03bda5bc2ca84f6c15fcd9cd57f30612922
jexeq <jerexand@gmail.com>
2021-08-04 13:55:15 -0300

    added redux

---
+++ client/src/REDUX/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+  
+// export function createProduct (userData) {
+//      return function(dispatch) {
+//          return axios.post(`http://localhost:3001/Product`, userData)
+//                         .then( (response) => {
+//                             dispatch({ type: "CREATE_USER", payload: response.data });
+//                         });
+//                 }                   
+// }
+
+export function LogInUser (userCredentials) {
+    return function (dispatch) {
+        return dispatch({type:"LOG_IN_USER", payload: userCredentials})
+    }
+}
