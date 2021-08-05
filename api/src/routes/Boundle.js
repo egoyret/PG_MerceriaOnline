a75be1b3ba70bdb08cd00f3f4e5150b8cc47a7d2
jhonntantb <jhonntantb@gmail.com>
2021-08-04 19:43:39 -0500

    PostmanBundle
    probandoBundle con postman
    crea y manda

--- api/src/routes/Boundle.js
+++ api/src/routes/Boundle.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { Bundle, Bundle_Product } = require('../db')
 
-router.get("/", (_req, res) => {
+router.get("/", (_req, res,next) => {
     Bundle.findAll()
     .then(bundles => res.status(200).send(bundles))
-    .catch(error => res.status(400).send(error))
+    .catch(error => next(error))
 })
 
 router.get("/:id", (req, res) => {
@@ -17,11 +17,24 @@ router.get("/:id", (req, res) => {
     .catch(error => res.status(400).send(error))
 })
 
-router.post("/", (req, res) => {
-    Bundle.create(req.body.bundle)
-    .then(bundle => Bundle_Product.create({bundle_id: bundle.id, product_id: req.body.product.id, quantity}))
-    .then(() => res.status(200).json("Creado exitosamente"))
-    .catch(error => res.status(400).send(error))
+router.post("/",async (req, res,next) => {
+    try {
+        const [bundle,created]=await Bundle.findOrCreate({
+            where:{title:req.body.title},
+            defaults:{
+                title:req.body.title,
+                discount:req.body.discount
+            }
+        });
+        res.send(bundle);
+        console.log(created);
+    } catch (error) {
+        next(error)
+    }
+    // Bundle.create(req.body.bundle)
+    // .then(bundle => Bundle_Product.create({bundle_id: bundle.id, product_id: req.body.product.id, quantity}))
+    // .then(() => res.status(200).json("Creado exitosamente"))
+    // .catch(error => res.status(400).send(error))
 })
 
 router.put("/", (req, res) => {
@@ -46,3 +59,4 @@ router.delete("/", (req, res) => {
     .then(() => res.status(200).json("Eliminado exitosamente"))
     .catch(error => res.status(400).send(error))
 })
+module.exports = router;
