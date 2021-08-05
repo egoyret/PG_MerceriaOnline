84c4a367582fd8cd44de836898b5afe735e2ac91
jhonntantb <jhonntantb@gmail.com>
2021-08-04 19:00:39 -0500

    Revert "probando Bundle"
    This reverts commit c2f091950b89676d798d02ebe964e4b68dbf9ac8.

--- api/src/routes/Boundle.js
+++ api/src/routes/Boundle.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Bundle, Bundle_Product } = require('../db')
-//por ahora solo llama y crea fata setear relaciones
+
 router.get("/", (_req, res) => {
     Bundle.findAll()
     .then(bundles => res.status(200).send(bundles))
@@ -17,17 +17,11 @@ router.get("/:id", (req, res) => {
     .catch(error => res.status(400).send(error))
 })
 
-router.post("/",async (req, res,next) => {
-    try {
-        const bundle=await Bundle.create(req.body)
-        res.send(bundle);
-    } catch (error) {
-        next(error)
-    }
-    // Bundle.create(req.body)
-    // .then(bundle => Bundle_Product.create({bundle_id: bundle.id, product_id: req.body.product.id, quantity}))
-    // .then(() => res.status(200).json("Creado exitosamente"))
-    // .catch(error => res.status(400).send(error))
+router.post("/", (req, res) => {
+    Bundle.create(req.body.bundle)
+    .then(bundle => Bundle_Product.create({bundle_id: bundle.id, product_id: req.body.product.id, quantity}))
+    .then(() => res.status(200).json("Creado exitosamente"))
+    .catch(error => res.status(400).send(error))
 })
 
 router.put("/", (req, res) => {
@@ -52,4 +46,3 @@ router.delete("/", (req, res) => {
     .then(() => res.status(200).json("Eliminado exitosamente"))
     .catch(error => res.status(400).send(error))
 })
-module.exports = router;
