9c7e9b57376dd527c4c84d4f74826a7506c30932
jhonntantb <jhonntantb@gmail.com>
2021-08-04 20:23:56 -0500

    postmanUser
    probando el User con postman
    crea un usuario
    envia el usuario creado
    revisar el propiedasd password

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,11 +5,13 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Office =require('./Office');
 const Bundle=require("./Boundle")
+const User=require("./User")
 const router = Router();
 
 router.use("/product", Product ) 
 router.use("/category", Category )
 router.use("/office", Office )
 router.use("/bundle",Bundle)
+router.use("/user",User)
 
 module.exports = router;
