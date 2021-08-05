f55ac20840f6ce3ed12740e36b00cdf46d3f3178
Matias Campodonico <campodonicomatias@hotmail.com>
2021-08-05 12:12:28 -0300

    actualizacion action 3

--- client/src/REDUX/actions/product/index.js
+++ client/src/REDUX/actions/product/index.js
@@ -1,38 +1,42 @@
 import axios from 'axios';
 import * as TYPES from "../types"
 
-export function createProduct(product){
-    return axios.post('http://localhost:3001/product', product)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.CREATE_PRODUCT, payload: response})
-        }
-    })
+
+export const createProduct = (product) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/product', product)
+        return dispatch({ type: TYPES.CREATE_PRODUCT, payload: json.data })
+    }
 }
 
-export function getProduct(id){
-    return axios.get('http://localhost:3001/product/' + id)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.GET_PRODUCT, payload: response})
-        }
-    })
+export const getProduct = (id) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/product/' + id)
+        return dispatch({ type: TYPES.GET_PRODUCT, payload: json.data })
+    }
 }
 
-export function getAllProduct(){
-    return axios.get('http://localhost:3001/product')
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.GET_ALL_PRODUCT, payload: response})
-        }
-    })
+
+export const getAllProduct = () => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/product')
+        return dispatch({ type: TYPES.GET_ALL_PRODUCT, payload: json.data })
+    }
 }
 
-export function updateProduct(params){
-    return axios.put('http://localhost:3001/product/', params)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.GET_PRODUCT, payload: response})
-        }
-    })
+
+export const updateProduct = (params) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/product/', params)
+        return dispatch({ type: TYPES.UPDATE_PRODUCT, payload: json.data })
+    }
 }
+
+
+export const deleteProduct = (params) => {
+    return async = (dispatch) => {
+        const json = await axios.delete('http://localhost:3001/product', params)
+        return dispatch({ type: TYPES.DELETE_PRODUCT, payload: json.data })
+    }
+}
+
--- client/src/REDUX/actions/user/index.js
+++ client/src/REDUX/actions/user/index.js
@@ -1,38 +1,37 @@
 import axios from 'axios';
 import * as TYPES from "../types"
 
-export function createUser(user){
-    return axios.post('http://localhost:3001/product', user)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.CREATE_USER, payload: response})
-        }
-    })
+export const createUser = (user) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/user', user)
+        return dispatch({ type: TYPES.CREATE_USER, payload: json.data })
+    }
 }
 
-export function updateUser(params){
-    return axios.put('http://localhost:3001/product', params)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.UPDATE_USER, payload: response})
-        }
-    })
+export const updateUser = (params) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/user', params)
+        return dispatch({ type: TYPES.UPDATE_USER, payload: json.data })
+    }
 }
 
-export function getUser(id){
-    return axios.get('http://localhost:3001/product/' + id)
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.GET_USER, payload: response})
-        }
-    })
+export const getUser = (id) => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/user/' + id)
+        return dispatch({ type: TYPES.GET_USER, payload: json.data })
+    }
 }
 
-export function getAllUser(){
-    return axios.get('http://localhost:3001/product')
-    .then((response) => {
-        return function (dispatch) {
-            return dispatch({type: TYPES.GET_ALL_USER, payload: response})
-        }
-    })
+export const getAllUser = () => {
+    return async = (dispatch) => {
+        const json = await axios.post('http://localhost:3001/user')
+        return dispatch({ type: TYPES.GET_ALL_USER, payload: json.data })
+    }
+}
+
+export const deleteUser = (params) => {
+    return async = (dispatch) => {
+        const json = await axios.delete('http://localhost:3001/user', params)
+        return dispatch({ type: TYPES.DELETE_USER, payload: json.data })
+    }
 }
